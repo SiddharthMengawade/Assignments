7667fc8b43250972de6728292401038c49a037ef
SiddharthMengawade <141357976+SiddharthMengawade@users.noreply.github.com>
2024-07-12 21:41:30 +0530

    Add files via upload

---
+++ Ass18program1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCoulmn)
+{
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i<=iRow; i++)
+    {
+        for(j = iCoulmn; j>=1; j--)
+        {
+           if(i==j)
+           {
+            printf("#\t");
+           }
+           else
+           {
+            printf("*\t");
+           }
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number rows and coulmn :\n");
+    scanf("%d%d", &iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
