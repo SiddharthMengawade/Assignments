9247481510714397cca96a32e671e869f4b80a5b
SiddharthMengawade <141357976+SiddharthMengawade@users.noreply.github.com>
2024-07-12 21:38:52 +0530

    Add files via upload

---
+++ Ass13program2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCoulmn)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow<=0 && iCoulmn<=0)
+    {
+        iRow = -iRow;
+        iCoulmn = -iCoulmn;
+    }
+
+    for(i = 1; i<=iRow; i++)
+    {
+        for(j = 1; j<=iCoulmn; j++)
+        {
+            printf("%d\t",j);
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows :\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of Coulmns :\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
---
+++ Ass13program4.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCoulmn)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow<=0 && iCoulmn<=0)
+    {
+        iRow = -iRow;
+        iCoulmn = -iCoulmn;
+    }
+
+    for(i = 1; i<=iRow; i++)
+    {
+        for(j = 1; j<=iCoulmn; j++)
+        {
+            if(j % 2 !=0)
+            {
+                printf("*\t");
+
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows and coulmn :\n");
+    scanf("%d%d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
