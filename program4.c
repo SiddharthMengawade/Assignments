8bd8db45686026fcb9d1baa10859ac1d8c652824
SiddharthMengawade <141357976+SiddharthMengawade@users.noreply.github.com>
2024-07-12 21:28:33 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+#define true 1
+#define false 0
+
+int Check(int iNo)
+{
+    if((iNo % 5) ==0)
+    {
+        return true;
+    }
+    else
+    {
+       return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number :\n");
+    scanf("%d", &iValue);
+
+    bRet = Check(iValue);
+
+    if(bRet == true)
+    {
+        printf("Divisible by 5");
+    }
+    else
+    {
+        printf("Not Divisible by 5");
+    }
+
+    return 0;
+}
---
+++ program5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int Accept(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt<=iNo; iCnt++)
+    {
+        printf("*");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: \n");
+    scanf("%d", &iValue);
+
+    Accept(iValue);
+    return 0;
+}
